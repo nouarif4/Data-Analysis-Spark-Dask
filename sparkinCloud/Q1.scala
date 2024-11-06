1147df3bbb8441bc0dc29658044c3f553ec4fed5
nouarif4 <98772219+nouarif4@users.noreply.github.com>
2024-11-06 14:00:31 +0300

    Create Q1.scala

---
+++ sparkinCloud/Q1.scala
@@ -0,0 +1,5 @@
+import org.jfree.chart.{ChartFactory, ChartPanel, ChartUtils}
+import org.jfree.chart.axis.{CategoryAxis, CategoryLabelPositions}
+import org.jfree.chart.plot.CategoryPlot
+import org.jfree.chart.renderer.category.BarRenderer
+import org.jfree.data.category.DefaultCategoryDataset
