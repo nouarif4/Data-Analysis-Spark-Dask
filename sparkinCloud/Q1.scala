59e2c72e249ffac44bf66e9009f47dceb1657476
nouarif4 <98772219+nouarif4@users.noreply.github.com>
2024-11-06 18:53:26 +0300

    Update Q1.scala

--- sparkinCloud/Q1.scala
+++ sparkinCloud/Q1.scala
@@ -1,5 +1,18 @@
-import org.jfree.chart.{ChartFactory, ChartPanel, ChartUtils}
-import org.jfree.chart.axis.{CategoryAxis, CategoryLabelPositions}
-import org.jfree.chart.plot.CategoryPlot
-import org.jfree.chart.renderer.category.BarRenderer
-import org.jfree.data.category.DefaultCategoryDataset
+import org.apache.spark.sql.functions._
+
+// Read data from Parquet file
+val df = spark.read.parquet("gs://datasetspark/DaskCleanedData.parquet")
+
+// Clean and process the data to calculate conversion rates, sorted by descending conversion_rate
+val conversionRates = df
+  .groupBy("category_code")
+  .agg(
+    count(when(col("event_type") === "purchase", true)).alias("purchases"),
+    count(when(col("event_type") === "view", true)).alias("views"),
+    (count(when(col("event_type") === "purchase", true)).cast("double") / count(when(col("event_type") === "view", true)).cast("double")).alias("conversion_rate")
+  )
+  .orderBy(desc("conversion_rate"))
+
+// Show conversion rates (Optional for debugging)
+conversionRates.show()
+
